3fab48ab124912546fdf70fc2a0b31afb4407fad
karadagserhat <71098921+karadagserhat@users.noreply.github.com>
2020-11-13 15:45:47 +0300

    Create KentSimgeleriDetailActivity.java

---
+++ KentSimgeleriDetailActivity.java
@@ -0,0 +1,32 @@
+package com.serhatkaradag.landmarkbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        TextView landmarkNameText = findViewById(R.id.LandmarkNameText);
+        TextView countryNameText = findViewById(R.id.CountryNameText);
+
+        Intent intent = getIntent();
+        String landmarkName = intent.getStringExtra("name");
+        String countryName = intent.getStringExtra("country");
+        landmarkNameText.setText(landmarkName);
+        countryNameText.setText(countryName);
+
+        Singleton singleton = Singleton.getInstance();
+        imageView.setImageBitmap(singleton.getChosenImage());
+
+
+    }
+}
