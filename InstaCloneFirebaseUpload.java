75b41df9530bf5e83463904042cb9484f10d6e4e
karadagserhat <71098921+karadagserhat@users.noreply.github.com>
2020-11-17 12:18:26 +0300

    Create InstaCloneFirebaseUpload.java

---
+++ InstaCloneFirebaseUpload.java
@@ -0,0 +1,163 @@
+package com.serhatkaradag.instaclonefirebase;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.UUID;
+
+public class UploadActivity extends AppCompatActivity {
+    Bitmap selectedImage;
+    ImageView imageView;
+    EditText commentText;
+
+    private FirebaseStorage firebaseStorage;
+    private StorageReference storageReference;
+    Uri imageData;
+
+    private FirebaseFirestore firebaseFirestore;
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+
+        imageView = findViewById(R.id.imageView);
+        commentText = findViewById(R.id.commentText);
+
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    public void upload(View view){
+        if(imageData != null) { //eğer kullanıcı bir şey seçtiyse bu işlemi yap
+
+            //universal unique id
+            UUID uuid = UUID.randomUUID();
+            String imageName = "images/" + uuid + ".jpeg";
+            //Storage da images klasörü altında random id lerle foto depolar.
+            storageReference.child(imageName).putFile(imageData).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    //eğer başarılı bir şekilde kaydedildiyse
+                    //Dowland Url
+                    StorageReference newReference = FirebaseStorage.getInstance().getReference(imageName); //nereye kaydedilğini bul
+                    newReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            String dowlandUrl = uri.toString();
+                            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                            String userEmail = firebaseUser.getEmail();
+                            String comment = commentText.getText().toString();
+
+                            HashMap<String, Object> postData = new HashMap<>();
+                            postData.put("useremail", userEmail);
+                            postData.put("dowlandurl", dowlandUrl);
+                            postData.put("comment", comment);
+                            postData.put("date", FieldValue.serverTimestamp()); //güncel zamanı verir
+                            firebaseFirestore.collection("Posts").add(postData).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Intent intent = new Intent(UploadActivity.this, FeedActivity.class);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); //bütün açık aktiviteleri kapat
+                                    startActivity(intent);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(UploadActivity.this, e.getLocalizedMessage().toString(),Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        }
+                    });
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(UploadActivity.this, e.getLocalizedMessage().toString(),Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+
+    public void selectImage(View view){
+        //Galeriye gitmek için önce izin istememiz gerekir. Eğer yoksa izin al.
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+        else{ //Eğer önceden izin verildiyse
+            Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intentToGallery, 2);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if( requestCode == 1){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){ //eğer izin veridiyse
+                Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intentToGallery, 2);
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == 2 && resultCode == RESULT_OK && data != null){
+            imageData = data.getData();
+            try {
+                if(Build.VERSION.SDK_INT >= 28){
+                    ImageDecoder.Source source = ImageDecoder.createSource(this.getContentResolver(), imageData);
+                    selectedImage = ImageDecoder.decodeBitmap(source);
+                    imageView.setImageBitmap(selectedImage);
+                }
+                else {
+                    selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageData);
+                    imageView.setImageBitmap(selectedImage);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
