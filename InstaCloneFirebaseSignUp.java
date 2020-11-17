5d8c87bf8dd99576c70ab174aef5b5138bdf5345
karadagserhat <71098921+karadagserhat@users.noreply.github.com>
2020-11-17 12:18:04 +0300

    Create InstaCloneFirebaseSignUp.java

---
+++ InstaCloneFirebaseSignUp.java
@@ -0,0 +1,84 @@
+package com.serhatkaradag.instaclonefirebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    EditText emailText, passwordText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        emailText = findViewById(R.id.emailText);
+        passwordText = findViewById(R.id.passwordText);
+
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if(firebaseUser != null){ //Kullanıcı zaten önceden giriş yaptıysa diğer sayfayı otomatik açacak
+            Intent intent = new Intent(SignUpActivity.this, FeedActivity.class);
+            startActivity(intent);
+            finish(); //finish bütün aktiviteyi yok eder. Yani geri tuşuna batığın zaman activiteye geri dönemezsin.
+        }
+
+    }
+
+    public void signInClick(View view){
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Intent intent = new Intent(SignUpActivity.this, FeedActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(SignUpActivity.this, e.getLocalizedMessage().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void signUpClick(View view){
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        /*if(email.matches("")){
+            AlertDialog
+        }*/
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(SignUpActivity.this, "User Created", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(SignUpActivity.this, FeedActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(SignUpActivity.this, e.getLocalizedMessage().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
